5f1350fb6e5a72f15a325c7606710fe0f2dd0755
bunny <67633271+bunnykek@users.noreply.github.com>
2022-07-05 22:06:08 +0530

    Update ddl.py

--- modules/ddl.py
+++ modules/ddl.py
@@ -11,12 +11,9 @@ URLRx = re.compile(r"(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%
 def ddlDownload(message, serviceID, progressMessage):
     print("ddlDownload", flush=True)
     ddl = URLRx.search(message.text).group(0)
-
-    try:
-        filename = requests.get(ddl).headers.get("Content-Disposition").split("filename=")[1].replace('"', '')
-        print(filename, flush=True)
-    except:
-        filename = ddl.split('/')[-1]
-    if not os.path.exists(os.path.join('Downloads', filename)):
-        subprocess.run(["curl", "-L", ddl, "-o", "Downloads/{}".format(filename)])
-    upload("Downloads/{}".format(filename), serviceID, message, progressMessage)
+    filename = os.path.basename(ddl).split("?")[0]
+    print(f"Downloading {filename}", flush=True)
+    filePath = os.path.join("Downloads", filename)
+    if not os.path.exists(filePath):
+        subprocess.run(["curl", "-L", ddl, "-o", filePath])
+    upload(filePath, serviceID, message, progressMessage)
