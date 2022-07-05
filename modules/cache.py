5bb418b801fed8b019c9daa6711faa8f162bfbc8
bunny <67633271+bunnykek@users.noreply.github.com>
2022-07-05 22:32:49 +0530

    Update cache.py

--- modules/cache.py
+++ modules/cache.py
@@ -6,7 +6,7 @@ import subprocess
 
 def CacheSize(msg):
     print("Getting cache size", flush=True)
-    output = subprocess.check_output(['du','-sh']).decode('utf-8')
+    output = subprocess.check_output(['du','-sh', 'Downloads']).split()[0].decode('utf-8')
     msg.reply_text("Cache size: " + output)
 
 def clearCache(msg):
